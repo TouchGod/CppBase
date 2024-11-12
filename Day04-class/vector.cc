7d5a65d30ef70694ace9d382632937973b1eb404
X1n <12409983+f-jiaxin@user.noreply.gitee.com>
2024-11-12 14:07:33 +0800

    vector

--- Day04-class/vector.cc
+++ Day04-class/vector.cc
@@ -7,53 +7,67 @@ using std::vector;
 class Point
 {
 public:
-    Point()
-        : _x(0), _y(0)
-    {}
+	/* Point() */
+	/* : _x(0), _y(0) */
+	/* {} */
 
-    Point(int x, int y)
-        : _x(x), _y(y)
-    {}
+	Point(int x = 0, int y = 0)
+	: _x(x), _y(y)
+	{
+		cout << "Point()" << endl;
+	}
 
-    void print() const
+	~Point()
+	{
+		cout << "~Point()" << endl;
+	}
+
+	Point(const Point & rhs)
+    : _x(rhs._x)
+    , _y(rhs._y)
     {
-        cout << "x=" << _x << ",";
-        cout << "y=" << _y << endl;
+        cout << "Point(const Point &)" << endl;
     }
 
+	void print() const
+	{
+		cout << "x=" << _x << ",";
+		cout << "y=" << _y << endl;
+	}
+
 private:
-    int _x;
-    int _y;
+	int _x;
+	int _y;
 };
 
 void test0()
 {
-    Point pt1;
-    Point pt2(1, 2);
+	Point pt1;
+	Point pt2(1, 2);
 
-    Point pt3(2, 5);
-    Point pt4(10, 20);
-    vector<Point> points{pt1, pt2, pt3};
-    // 下标方式遍历
-    for (int idx = 0; idx < points.size(); ++idx)
-    {
-        points[idx].print();
-    }
-    points.push_back(pt4);
-    points.push_back({5, 8});
-    // 迭代器方式遍历
-    for (auto it = points.begin(); it != points.end(); ++it)
-    {
-        (*it).print();
-        it->print();
-    }
+	Point pt3(2, 5);
+	Point pt4(10, 20);
+	vector<Point> points{pt1, pt2, pt3};
+	// 下标方式遍历
+	for (size_t idx = 0; idx < points.size(); ++idx)
+	{
+		points[idx].print();
+	}
+	points.push_back(pt4);
+	points.push_back({5, 8});
+	// 迭代器方式遍历
+	for (auto it = points.begin(); it != points.end(); ++it)
+	{
+		(*it).print();
+		it->print();
+	}
 
-    vector<Point> p2{{1, 1}, {2, 2}, {3, 3}};
-    // 增强for循环
-    for (auto &pt : p2)
-    {
-        pt.print();
-    }
+	vector<Point> p2{{1, 1}, {2, 2}, {3, 3}};
+	// 增强for循环
+	for (auto &pt : p2)
+	{
+		pt.print();
+	}
 }
 
 void test1(){
@@ -69,9 +83,27 @@ void test1(){
 	}
 }
 
+void test2(){
+	// 隐式转换
+	// 临时构造了一个Point(4,0)
+	// 再调用三次拷贝构造
+	// 保存到vector中
+	// 临时Point对象销毁
+	vector<Point> nums3(3,4);
+	for(auto & ele : nums3){
+		ele.print();
+	}
+
+	cout << endl;
+	vector<Point> nums4{3,4};
+	for(auto & ele : nums4){
+		ele.print();
+	}
+}
 int main()
 {
-    test0();
-    test1();
-    return 0;
+	/* test0(); */
+	/* test1(); */
+	test2();
+	return 0;
 }
---
+++ Day05-stream/strongInt_v1.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <sstream>
+#include <limits>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void test0(){
+	cout << "Please input a int num" << endl;
+	int num;
+	cin >> num;
+	while(!cin.eof()){
+		if(cin.bad()){
+			cout << "There is a broken stream" << endl;
+			return ;
+		}
+		else if (cin.fail()){
+			// 重置流状态
+			cin.clear();
+			// 清除缓冲
+			cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+			cout << "Please input a int num" << endl;
+			cin >> num;
+		}
+		else {
+			cout << "num is " << num << endl;
+			break;
+		}
+	}
+}
+
+void test1(){
+	cout << "Please input a int num" << endl;
+	int num;
+	// 逗号表达式的使用
+	while(cin >> num, !cin.eof()){
+		if(cin.bad()){
+			cout << "There is a broken stream" << endl;
+			return ;
+		}
+		else if (cin.fail()){
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+			cout << "Please input a int num" << endl;
+		}
+		else {
+			cout << "num is " << num << endl;
+			break;
+		}
+	}
+}
+
+int main()
+{
+	test0();
+    return 0;
+}
+
