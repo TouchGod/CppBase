4d936a2ec2bdf1101d92b69752a826c25fb1a11b
X1n <12409983+f-jiaxin@user.noreply.gitee.com>
2024-11-13 20:19:36 +0800

    strongInt v2

--- Day05-stream/strongInt.cc
+++ Day05-stream/strongInt.cc
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <sstream>
 #include <limits>
+#include <string>
 using std::cin;
 using std::cout;
 using std::endl;
+using std::istringstream;
+using std::string;
 
 void test0()
 {
@@ -60,8 +63,46 @@ void test1()
 	}
 }
 
+void test2()
+{
+	cout << "Please input a int num" << endl;
+	int num;
+	string s_num;
+	while (cin >> s_num, !cin.eof())
+	{
+		if (cin.bad())
+		{
+			cout << "There is a broken stream" << endl;
+			return;
+		}
+		bool flag = true;
+		for (auto &n : s_num)
+		{
+			if (n < '0' || n > '9')
+			{
+				flag = false;
+			}
+		}
+
+		if (flag)
+		{
+			istringstream iss(s_num);
+			iss >> num;
+			cout << "The num is " << num << endl;
+			break;
+		}
+		else
+		{
+			cout << "Not int, try again." << endl;
+			s_num.clear();
+		}
+	}
+}
+
 int main()
 {
-	test0();
+	// test0();
+	// test1();
+	test2();
 	return 0;
 }
